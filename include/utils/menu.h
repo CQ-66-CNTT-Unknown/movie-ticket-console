1411f10c046328e22632e498bc51959ff8a2b62e
TTNguyen2505 <trantrongnguyen2552003@gmail.com>
2026-05-01 22:08:53 +0700

    refactor: rename show_meu_actions to menu

--- include/utils/menu.h
+++ include/utils/menu.h
@@ -3,7 +3,7 @@
 #include "../models/user.h"
 
 /**
- * Show the actions menu based on the user's role
+ *@brief Show the actions menu based on the user's role
  *@param role The role of the user (MANAGER or CUSTOMER)
  */
 void show_actions_menu(UserRole role);
