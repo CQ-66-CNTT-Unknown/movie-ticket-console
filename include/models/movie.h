e95bbdf05c6da26e15288fac4f7da48c7eb0fcbd
TTNguyen2505 <trantrongnguyen2552003@gmail.com>
2026-05-02 20:58:02 +0700

    feat: add dynamic array structures for movies and screenings

--- include/models/movie.h
+++ include/models/movie.h
@@ -8,3 +8,12 @@ typedef struct Movie {
     char title[100]; /**< The title of the movie */
     int duration; /**< The duration of the movie in minutes */
 } Movie;
+
+/**
+ *@brief Dynamic array structure to hold multiple movies
+ */
+typedef struct MovieArray {
+    Movie *movies; /**< Pointer to an array of Movie structures */
+    int count; /**< The number of movies currently in the array */
+    int capacity; /**< The maximum number of movies the array can hold */
+} MovieArray;
